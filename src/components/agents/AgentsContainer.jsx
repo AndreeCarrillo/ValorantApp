a6925bc252b1aaef166c542fa3d3fced8b4a9459
AndreeCarrillo <u20211b589@upc.edu.pe>
2023-08-05 15:02:57 -0500

    last version 2.0

--- src/components/agents/AgentsContainer.jsx
+++ src/components/agents/AgentsContainer.jsx
@@ -27,11 +27,11 @@ export const AgentsContainer = ({item, role,index}) => {
                 <div className={index%2==0?"principal d-flex flex-row align-items-end py-5":"principal d-flex flex-row-reverse align-items-start py-5"} style={index%2===0?{backgroundColor:`#${item.backgroundGradientColors[0]}`, marginInline:"3%"}:{backgroundColor:`#${item.backgroundGradientColors[0]}`, margin:"3%"}}>
                     <div className='ghost' style={{width:"60%"}}></div>
                     <div className="padding px-4" style={index%2 == 0 ? {width:"25%", backgroundColor:`#${item.backgroundGradientColors[0]}`, marginRight:"10%"}:{width:"25%", backgroundColor:`#${item.backgroundGradientColors[0]}`, marginLeft:"10%"}}>
-                        <h1 className='d-flex justify-content-center' style={{fontFamily:"'Saira Condensed', sans-serif", fontSize:"7vw", color:"#111"}}>{item.displayName.toUpperCase()}</h1>
+                        <h1 className='d-flex justify-content-center' style={{fontFamily:"'Saira Condensed', sans-serif", fontSize:"5em", color:"#111"}}>{item.displayName.toUpperCase()}</h1>
                         <hr className='d-flex justify-content-center' style={{border:`1.5px solid #111`, width:"100%"}}></hr>
                         <h5 className="py-0" style={{fontFamily:"'Saira Condensed', sans-serif", color:"#111"}}>// ROL</h5>
                         <div className="div2 d-flex flex-row align-items-stretch pb-2">
-                            <h1 className="" style={{fontFamily:"'Saira Condensed', sans-serif", fontSize:"5vw", color:"#111"}}>{role.displayName.toUpperCase()}</h1>
+                            <h1 className="" style={{fontFamily:"'Saira Condensed', sans-serif", fontSize:"5em", color:"#111"}}>{role.displayName.toUpperCase()}</h1>
                             <img className="imgb mx-3 mt-3" style={{width:"2em", height:"2em", color:"#111"}} src={role.displayIcon}/>
                         </div>
                         <h5 style={{fontFamily:"'Saira Condensed', sans-serif", color:"#111"}}>// BIOGRAFÍA</h5>
